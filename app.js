b7327931d5119bfb61f6cc4ab59708f9eb6c3e28
MayurMallavSaikia <89935072+MayurMallavSaikia@users.noreply.github.com>
2022-08-18 11:11:49 +0530

    added like and dislike module

--- app.js
+++ app.js
@@ -31,6 +31,12 @@ mongoose
   require("./model/Favorite")                         // Favorite Model Import
   const Favorite=mongoose.model("favorites");
 
+   require("./model/Like")                            //Like Model Import
+   const Like=mongoose.model("likes")
+
+   require("./model/Dislike")                         // Dislike Model Import
+   const Dislike=mongoose.model("dislikes")
+
   
 
   /*
@@ -179,6 +185,140 @@ app.post("/api/favorite/getFavoredMovie", (req, res) => {
             return res.status(200).json({ success: true, favorites })
         })
 });
+
+
+
+/*
+    Like and Dislike API calls
+  */
+
+    app.post("/api/like/getLikes", (req, res) => {
+
+      let variable = {}
+      if (req.body.videoId) {
+          variable = { videoId: req.body.videoId }
+      } else {
+          variable = { commentId: req.body.commentId }
+      }
+  
+      Like.find(variable)
+          .exec((err, likes) => {
+              if (err) return res.status(400).send(err);
+              res.status(200).json({ success: true, likes })
+          })
+  
+  
+  })
+  
+  
+  app.post("/api/like/getDislikes", (req, res) => {
+  
+      let variable = {}
+      if (req.body.videoId) {
+          variable = { videoId: req.body.videoId }
+      } else {
+          variable = { commentId: req.body.commentId }
+      }
+  
+      Dislike.find(variable)
+          .exec((err, dislikes) => {
+              if (err) return res.status(400).send(err);
+              res.status(200).json({ success: true, dislikes })
+          })
+  
+  })
+  
+  
+  app.post("/api/like/upLike", (req, res) => {
+  
+      let variable = {}
+      if (req.body.videoId) {
+          variable = { videoId: req.body.videoId, userId: req.body.userId }
+      } else {
+          variable = { commentId: req.body.commentId , userId: req.body.userId }
+      }
+  
+      const like = new Like(variable)
+      //save the like information data in MongoDB
+      like.save((err, likeResult) => {
+          if (err) return res.json({ success: false, err });
+          //In case disLike Button is already clicked, we need to decrease the dislike by 1 
+          Dislike.findOneAndDelete(variable)
+              .exec((err, disLikeResult) => {
+                  if (err) return res.status(400).json({ success: false, err });
+                  res.status(200).json({ success: true })
+              })
+      })
+  
+  })
+  
+  
+  
+  
+  app.post("/api/like/unLike", (req, res) => {
+  
+      let variable = {}
+      if (req.body.videoId) {
+          variable = { videoId: req.body.videoId, userId: req.body.userId }
+      } else {
+          variable = { commentId: req.body.commentId , userId: req.body.userId }
+      }
+  
+      Like.findOneAndDelete(variable)
+          .exec((err, result) => {
+              if (err) return res.status(400).json({ success: false, err })
+              res.status(200).json({ success: true })
+          })
+  
+  })
+  
+  
+  app.post("/api/like/unDisLike", (req, res) => {
+  
+      let variable = {}
+      if (req.body.videoId) {
+          variable = { videoId: req.body.videoId, userId: req.body.userId }
+      } else {
+          variable = { commentId: req.body.commentId , userId: req.body.userId }
+      }
+  
+      Dislike.findOneAndDelete(variable)
+      .exec((err, result) => {
+          if (err) return res.status(400).json({ success: false, err })
+          res.status(200).json({ success: true })
+      })
+  
+  
+  })
+  
+  
+  
+  app.post("/api/like/upDisLike", (req, res) => {
+  
+      let variable = {}
+      if (req.body.videoId) {
+          variable = { videoId: req.body.videoId, userId: req.body.userId }
+      } else {
+          variable = { commentId: req.body.commentId , userId: req.body.userId }
+      }
+  
+      const disLike = new Dislike(variable)
+      //save the like information data in MongoDB
+      disLike.save((err, dislikeResult) => {
+          if (err) return res.json({ success: false, err });
+          //In case Like Button is already clicked, we need to decrease the like by 1 
+          Like.findOneAndDelete(variable)
+              .exec((err, likeResult) => {
+                  if (err) return res.status(400).json({ success: false, err });
+                  res.status(200).json({ success: true })
+              })
+      })
+  
+  
+  })
+  
+
+
 
 
 
